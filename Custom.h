ecf36648c2093cff32ceb343d5150edba776f98a
Ujwal Komarla <ujwal_komarla@ncsu.edu>
2014-12-01 12:32:35 -0500

    Some modifications with defines

--- Custom.h
+++ Custom.h
@@ -6,7 +6,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <linux/if_link.h>
+#include<pthread.h>
+#include<signal.h>
 
+#define itimerspec linux_itimerspec
+#define timespec linux_timespec
+//#define timeval linux_timeval
+#include<sys/time.h>
+//#undef timeval
+#undef timespec
+#undef itimerspec
+#define INFINITY 99.9
 
 int getMyIp(void);
 void DieWithError(char *);
